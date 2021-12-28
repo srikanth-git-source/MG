4fb9d30fbe46a1bce3a6ecddc8ebda360905d20f
Anji <Anji@optionmatrix.com>
2021-12-28 13:17:27 +0530

    dashoard

--- src/app/new-dashboard/overview/astor/astor.module.ts
+++ src/app/new-dashboard/overview/astor/astor.module.ts
@@ -5,18 +5,21 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { DashboardChecklistAuditComponent } from './dashboard-checklist-audit/dashboard-checklist-audit.component';
 import { NgApexchartsModule } from 'ng-apexcharts';
+import { DashboardParameterAuditComponent } from './dashboard-parameter-audit/dashboard-parameter-audit.component';
+import { ParameterDashboardComponent } from 'src/app/parameter-dashboard/parameter-dashboard.component';
 
 
 export const routes = [
   {path:'',component:DashboardChecklistAuditComponent,data:{breadcrumb:'Dashboard'}},
   // { path:'astor',component:AstorComponent, data: { breadcrumb: 'Dashboard' } },
   { path:'dashboard-checklist-audit',component:DashboardChecklistAuditComponent, loadChildren: './dashboard-checklist-audit/dashboard-checklist-audit.module#DashboardChecklistAuditModule', data: { breadcrumb: 'Dashboard' } },
+  { path:'dashboard-parameter-audit',component:DashboardParameterAuditComponent, loadChildren: './dashboard-parameter-audit/astor-parameter-audit.module#AstorParameterAuditModule', data: { breadcrumb: 'Dashboard' } },
 ]
 
 
 
 @NgModule({
-  declarations: [ DashboardChecklistAuditComponent],
+  declarations: [ DashboardChecklistAuditComponent, DashboardParameterAuditComponent],
   imports: [
     CommonModule,RouterModule.forChild(routes),SharedModule,NgApexchartsModule
   ]
---
+++ src/app/new-dashboard/overview/astor/dashboard-parameter-audit/dashboard-parameter-audit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-parameter-audit',
+  templateUrl: './dashboard-parameter-audit.component.html',
+  styleUrls: ['./dashboard-parameter-audit.component.scss']
+})
+export class DashboardParameterAuditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
