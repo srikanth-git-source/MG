145eb1256653f545040ddbe3dffdb7445440c78c
Anji <Anji@optionmatrix.com>
2021-12-29 16:44:35 +0530

    changes

---
+++ src/app/new-dashboard/quality-audit/qa-astor/dashboard-qa-parameter-audit/dashboard-qa-parameter-audit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-qa-parameter-audit',
+  templateUrl: './dashboard-qa-parameter-audit.component.html',
+  styleUrls: ['./dashboard-qa-parameter-audit.component.scss']
+})
+export class DashboardQaParameterAuditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
