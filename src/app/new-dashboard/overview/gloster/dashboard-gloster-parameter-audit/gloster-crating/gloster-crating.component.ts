c9afe563f79bfa46b90c3eef077dc3ec63d82da9
Anji <Anji@optionmatrix.com>
2021-12-29 11:10:39 +0530

    overview rating design

--- src/app/new-dashboard/overview/gloster/dashboard-gloster-parameter-audit/gloster-crating/gloster-crating.component.ts
+++ src/app/new-dashboard/overview/gloster/dashboard-gloster-parameter-audit/gloster-crating/gloster-crating.component.ts
@@ -1,4 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ChartComponent,
+  ApexDataLabels,
+  ApexXAxis,
+  ApexPlotOptions,
+  ApexStroke
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+  xaxis: ApexXAxis;
+  stroke: ApexStroke;
+};
 
 @Component({
   selector: 'app-gloster-crating',
@@ -7,9 +25,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GlosterCratingComponent implements OnInit {
 
-  constructor() { }
+  filterToggle: boolean;
+  @ViewChild("chart") chart: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
+  constructor() { 
+    this.chartOptions = {
+      series: [
+        {
+          name: "Monthly Avg Defect Rate",
+          data: [125, 140]
+        },
+        {
+          name: "Weekly Avg Defect Rate",
+          data: [105, 95]
+        },
+        {
+          name: "Target Limit",
 
+          data: [90, 51]
+        },
+        // {
+        //   name: "serie3",
+
+        //   data: [53, 32, 33]
+        // },
+        // {
+        //   name: "serie3",
+
+        //   data: [53, 32, 33]
+        // }
+      ],
+      chart: {
+        type: "bar",
+        height: 430
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true,
+          dataLabels: {
+            position: "top"
+          }
+        }
+      },
+      dataLabels: {
+        enabled: true,
+        offsetX: -6,
+        style: {
+          fontSize: "12px",
+          colors: ["#fff"]
+        }
+      },
+      stroke: {
+        show: false,
+        width: 1,
+        colors: ["#fff"]
+      },
+      xaxis: {
+        categories: ["Interior",  "Exterior"]
+      }
+    };
+
+    
+  }
+  MonthSelected = 11;
+  Yearselected = 0;
   ngOnInit() {
   }
+
+
+  items = [
+    {checkpoints:"1A - HOOD TO FRONT GRILL- GAP( a )"}, 
+    {checkpoints:"1A - HOOD TO FRONT GRILL - GAP( b )"}, 
+    {checkpoints:"1 - TAILGATE TRIM TO DECORATIVE PANEL - FLUSH"}, 
+    {checkpoints:"TAILGATE TRIM to TAILGATE GLASS BLACK EDGE - PARALLELISM"}, 
+    {checkpoints:"6 - TAILGATE TRIM to TAILGATE GLASS BLACK EDGE - DIST"},
+    
+    ];
 
 }
